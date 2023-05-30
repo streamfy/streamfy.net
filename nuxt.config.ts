2a1aae192a40d7e34c5c0f52a110496bbe67d45f
Muhammad <124201331+t4-muhammad@users.noreply.github.com>
2023-05-30 14:45:04 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   modules: [
     [
       "@vee-validate/nuxt",
@@ -26,8 +27,4 @@ export default defineNuxtConfig({
     // "github:traderfour/base-trader4-layers", // Extend from a git repository
     "base-layers", // Extend from a local directory
   ],
-  routeRules: {
-    // Render these routes with SPA
-    "/my/**": { ssr: false },
-  },
 });
